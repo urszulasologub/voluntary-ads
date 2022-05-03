1fa173f5a0dfe0dd17b23138c124c58e0ddf5d3c
urszulasologub <gold.ula4@gmail.com>
2022-05-03 19:58:12 +0200

    Add a chart

--- sell-announcements/app/src/Pages/AdminPage.jsx
+++ sell-announcements/app/src/Pages/AdminPage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import RootTemplate from 'templates/RootTemplate';
 import CategoryList from 'Components/Category/CategoryList';
 import CreateAdminAndDb from 'Components/Admin/CreateAdminAndDb';
+import Chart from 'Components/Chart/Chart';
 
 const AdminPage = () => {
   return (
     <RootTemplate>
+      <Chart />
       <CategoryList />
       <CreateAdminAndDb />
     </RootTemplate>
